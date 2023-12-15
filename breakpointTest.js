301cd961bdcfe647289697cdb83ccea3849f53a6
Christopher Owusu <120009994+Chris-Owusu@users.noreply.github.com>
2023-12-15 16:21:47 +0000

    initail commit for K6 perfomance testing

---
+++ breakpointTest.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    stages: [
+        {
+            duration: '4h',
+            target: 1000000
+        }
+    ]
+}
+
+export default function() {
+    http.get('https://test.k6.io');
+    sleep(1);
+}
