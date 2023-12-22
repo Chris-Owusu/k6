d8dda22c353ffc0d6396cda6a093722a1176e383
Chris-Owusu <christopher.owusu@amalitech.com>
2023-12-22 09:53:48 +0000

    assertions in k6

---
+++ assertions.js
@@ -0,0 +1,10 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+
+export default function () {
+    http.get("https://test.k6.io")
+    check(true, {
+        "true is true": (value) => value === true
+    });
+}
