afaaa08afa97450d2ec663e6da6312c3a2fa25f8
Chris-Owusu <christopher.owusu@amalitech.com>
2024-01-19 12:05:38 +0000

    post request

---
+++ postRequest.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export default function() {
+    const body = JSON.stringify({
+        "username": "test_" + Date.now(),
+        "first_name": "Bon",
+        "last_name": "go",
+        "email": "bon@gmail.com",
+        "password": "3423423423"
+    })
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    http.post("https://test-api.k6.io/user/register/", body, params);
+}
